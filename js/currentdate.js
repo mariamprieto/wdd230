51c30d700dfb9c09ae4f841dfd0583bdbe6aa152
¨mariamprieto¨ <mariaprieto@byui.edu>
2022-04-26 10:01:38 -0400

    Date bug fixes

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,17 @@
-const date = { month: "numeric", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second:"numeric"};
-document.getElementById('currentdate').textContent =new Date().toLocaleDateString('en-US', date);
+const date = {
+  month: "numeric",
+  day: "numeric",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+document.getElementById("currentdate").textContent = new Date(
+  document.lastModified
+).toLocaleDateString("en-US", date);
 
+const currentyear = {
+  year: "numeric",
+    };
+document.getElementById("currentyear").textContent =
+  new Date().toLocaleDateString("en-US", currentyear);
