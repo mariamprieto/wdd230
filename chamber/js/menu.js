e56b711ed9db5da0a02b344ac052ad8ea91d3e7a
mariamprieto <mariaprieto@byui.edu>
2022-05-14 23:01:17 -0400

    update

---
+++ chamber/js/menu.js
@@ -0,0 +1,36 @@
+const lastdate = {
+  month: "numeric",
+  day: "numeric",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+document.getElementById("lastupdated").textContent = new Date(
+  document.lastModified
+).toLocaleDateString("en-US", lastdate);
+
+
+
+
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+datefield.innerHTML = fulldate;
+
+
+const currentyear = {
+  year: "numeric",
+};
+document.getElementById("currentyear").textContent =
+  new Date().toLocaleDateString("en-US", currentyear);
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburger").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburger");
+x.onclick = toggleMenu;
