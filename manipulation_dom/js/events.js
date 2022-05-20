b8be60326750431ce4439d5dbe5d6a88d244d1d6
mariamprieto <mariaprieto@byui.edu>
2022-05-19 23:36:38 -0400

    Update

--- manipulation_dom/js/events.js
+++ manipulation_dom/js/events.js
@@ -1,52 +1,37 @@
-const input = document.querySelector('input');
-const button_read = document.querySelector('button');
-const list = document.querySelector('ul');
+const input = document.querySelector("input");
+const button_read = document.querySelector("button");
+const list = document.querySelector("ul");
 
+button_read.addEventListener("click", () => {
+  const texto = input.value;
+  input.value = "";
+  input.focus();
+  let span = document.createElement("span");
+  span.textContent = texto;
 
-button_read.addEventListener('click', () => {
-    const texto = input.value;
-    input.value = "";
-    input.focus();
-    let span = document.createElement("span");
-    span.textContent = texto;
-    
-    let btn = document.createElement("button");
-    btn.textContent = "X"
-    btn.style.backgroundColor = "#FAEBD7";
-    btn.style.margin = "10px";
-    btn.style.padding = "3px";
-    btn.style.color = "red";
-    btn.style.fontWeight = "bolder";
-    btn.style.gridColumn = "2/3";
-    btn.style.width = "30px";
-    btn.style.height = "30px";
-    
+  let btn = document.createElement("button");
+  btn.textContent = "X";
+  btn.style.backgroundColor = "#FAEBD7";
+  btn.style.margin = "10px";
+  btn.style.padding = "3px";
+  btn.style.color = "red";
+  btn.style.fontWeight = "bolder";
+  btn.style.gridColumn = "2/3";
+  btn.style.width = "30px";
+  btn.style.height = "30px";
 
+  let listItem = document.createElement("li");
+  listItem.appendChild(span);
+  listItem.appendChild(btn);
+  list.appendChild(listItem);
+  list.setAttribute(
+    "style",
+    "list-style:none;marginTop:20px;width:300px;backgroundColor:;border:1px solid blue;"
+  );
+  span.style.textAlign = "rigth";
+  span.style.padding = "7px";
 
-
-    let listItem = document.createElement("li");
-    listItem.appendChild(span);
-    listItem.appendChild(btn);
-    list.appendChild(listItem);
-    list.setAttribute(
-      "style",
-      "list-style:none;marginTop:20px;width:300px;backgroundColor:;border:1px solid blue;"
-    );
-    
-    span.style.textAlign = "rigth";
-    span.style.padding = "7px";
-    
-
-    
-
- btn.addEventListener("click", function () {
-     list.removeChild(listItem);
-     
- }) ;
-   
-    
-   
+  btn.addEventListener("click", function () {
+    list.removeChild(listItem);
+  });
 });
-
-
-
