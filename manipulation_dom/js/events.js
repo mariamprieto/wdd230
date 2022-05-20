3401c77164b4160d5d40bbc651bc567455356466
mariamprieto <mariaprieto@byui.edu>
2022-05-19 23:33:49 -0400

    Udpate

--- manipulation_dom/js/events.js
+++ manipulation_dom/js/events.js
@@ -12,13 +12,31 @@ button_read.addEventListener('click', () => {
     
     let btn = document.createElement("button");
     btn.textContent = "X"
-   
+    btn.style.backgroundColor = "#FAEBD7";
+    btn.style.margin = "10px";
+    btn.style.padding = "3px";
+    btn.style.color = "red";
+    btn.style.fontWeight = "bolder";
+    btn.style.gridColumn = "2/3";
+    btn.style.width = "30px";
+    btn.style.height = "30px";
+    
+
 
 
     let listItem = document.createElement("li");
     listItem.appendChild(span);
     listItem.appendChild(btn);
     list.appendChild(listItem);
+    list.setAttribute(
+      "style",
+      "list-style:none;marginTop:20px;width:300px;backgroundColor:;border:1px solid blue;"
+    );
+    
+    span.style.textAlign = "rigth";
+    span.style.padding = "7px";
+    
+
     
 
  btn.addEventListener("click", function () {
