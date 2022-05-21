6c62aa364c2ab3c3d2b0e0c003b92b201a5e17ef
mariamprieto <mariaprieto@byui.edu>
2022-05-21 18:58:00 -0400

    udpate

--- manipulation_dom/js/events.js
+++ manipulation_dom/js/events.js
@@ -4,34 +4,33 @@ const list = document.querySelector("ul");
 
 button_read.addEventListener("click", () => {
   const texto = input.value;
-  input.value = "";
-  input.focus();
-  let span = document.createElement("span");
-  span.textContent = texto;
+  if (texto != "") {
+    input.focus();
+    let listItem = document.createElement("li");
+    let span = document.createElement("span");
+    span.textContent = texto;
+    let btn = document.createElement("button");
+    btn.textContent = "X";
 
-  let btn = document.createElement("button");
-  btn.textContent = "X";
-  btn.style.backgroundColor = "#FAEBD7";
-  btn.style.margin = "10px";
-  btn.style.padding = "3px";
-  btn.style.color = "red";
-  btn.style.fontWeight = "bolder";
-  btn.style.gridColumn = "2/3";
-  btn.style.width = "30px";
-  btn.style.height = "30px";
+    listItem.appendChild(span);
+    listItem.appendChild(btn);
+    list.appendChild(listItem);
+    btn.style.backgroundColor = "#FAEBD7";
+    btn.style.margin = "5px";
+    btn.style.padding = "3px";
+    btn.style.color = "red";
+    btn.style.fontWeight = "bolder";
+    btn.style.width = "30px";
+    btn.style.height = "30px";
+    
+    
+    
+    btn.addEventListener("click", function () {
+      list.removeChild(listItem);
+    });
+  } 
+     
 
-  let listItem = document.createElement("li");
-  listItem.appendChild(span);
-  listItem.appendChild(btn);
-  list.appendChild(listItem);
-  list.setAttribute(
-    "style",
-    "list-style:none;marginTop:20px;width:300px;backgroundColor:;border:1px solid blue;"
-  );
-  span.style.textAlign = "rigth";
-  span.style.padding = "7px";
 
-  btn.addEventListener("click", function () {
-    list.removeChild(listItem);
-  });
+  
 });
