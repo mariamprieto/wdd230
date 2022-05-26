c8e87a458cf8be2e912c3d3909209fc0d8fc7144
mariamprieto <mariaprieto@byui.edu>
2022-05-25 23:41:02 -0400

    update

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -34,3 +34,17 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburger");
 x.onclick = toggleMenu;
+
+
+const message = document.querySelector(".message");
+const day = new Date().getDay();
+if (day <= 2) {
+  message.textContent =
+    "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+} else if (day == 3) {
+  message.textContent =
+    "Come join us for the chamber meet and greet tonight at 7:00 p.m.";
+} else {
+  message.textContent =
+    "We're sorry you missed the weekly chamber meet and greet but feel free to join us next Wednesday at 7:00 p.m.";
+}
