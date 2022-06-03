bee5fd247d9a32cf0c601569dfd027387908bca8
mariamprieto <mariaprieto@byui.edu>
2022-06-03 18:10:28 -0400

    update again again

--- chamber/js/message.js
+++ chamber/js/message.js
@@ -1,11 +1,8 @@
 const message = document.querySelector(".message");
 const day = new Date().getDay();
-if (day <= 2) {
+if (day === 1 || day === 2) {
   message.textContent =
     "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-} else if (day == 3) {
-  message.textContent =
-    "Come join us for the chamber meet and greet tonight at 7:00 p.m.";
 } else {
   message.textContent =
     "We're sorry you missed the weekly chamber meet and greet but feel free to join us next Wednesday at 7:00 p.m.";
