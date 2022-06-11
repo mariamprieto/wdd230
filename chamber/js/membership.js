843ddc5b1ed698c4e3d0dd8378d33a5f4a27e1e1
mariamprieto <mariaprieto@byui.edu>
2022-06-11 17:24:40 -0400

    udpate

--- chamber/js/membership.js
+++ chamber/js/membership.js
@@ -1,27 +1,26 @@
 function changeStatus() {
-    var status_member = document.getElementById("membership");
-    
-    if (status_member.value === "nonprofit") {
-        document.getElementById("nonprofit").style.display = "block";
-       
-    }
-    if (status_member.value === "bronze") {
-        document.getElementById("bronze").style.display = "block";
-    }
-    if (status_member.value === "silver") {
-      document.getElementById("silver").style.display = "block";
-    }
-    if (status_member.value === "gold") {
-      document.getElementById("gold").style.display = "block";
-    }
-    else {
-        document.getElementById("gold").style.display = "none";
-        document.getElementById("silver").style.display = "none";
-        document.getElementById("bronze").style.display = "none";
-        document.getElementById("nonprofit").style.display = "none";
-    }
+  var status_member = document.getElementById("membership");
   
+  
+  if (status_member.value === "nonprofit") {
+    document.getElementById("nonprofit").style.display = "block";
+
+  }
+  if (status_member.value === "bronze") {
+    document.getElementById("bronze").style.display = "block";
+  }
+  if (status_member.value === "silver") {
+    document.getElementById("silver").style.display = "block";
+  }
+  if (status_member.value === "gold") {
+    document.getElementById("gold").style.display = "block";
+  }
+  else {
+    document.getElementById("benefits").style.display = "none";
+  }
 
 
-    
+
+
+
 }
