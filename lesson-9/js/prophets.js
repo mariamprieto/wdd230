58fb36d2ffdd23c290a72e639ca7a5063ddee7fd
mariamprieto <mariaprieto@byui.edu>
2022-06-16 19:07:31 -0400

    udpate

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -14,21 +14,26 @@ async function getInformation() {
 }
 
 function buildCard(data) {
+    console.log(data);
 
     data.prophets.forEach(prophet => {
         let card = document.createElement('div');
         let h2 = document.createElement('h2');
         let p = document.createElement('p');
+        let place = document.createElement('p');
         let imagen = document.createElement('img');
         let fullName = `${prophet.name} ${prophet.lastname}`;
 
         h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
         p.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+        place.innerHTML = `Place of Birth: ${prophet.birthplace}`;
         imagen.setAttribute('src', prophet.imageurl);
         imagen.setAttribute('alt', `Picture of Prophets ${fullName}`);
+        imagen.setAttribute('loading', 'lazy');
 
         card.append(h2);
         card.append(p);
+        card.append(place);
         card.append(imagen);
         
         containerCard.append(card);
