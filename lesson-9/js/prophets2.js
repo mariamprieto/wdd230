00654595243dc2a8694acd715f1f594b5aa3ba42
mariamprieto <mariaprieto@byui.edu>
2022-06-16 19:20:13 -0400

    update

--- lesson-9/js/prophets2.js
+++ lesson-9/js/prophets2.js
@@ -1,13 +1,12 @@
 const URL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
-const containerCard = document.getElementById("cards");
+const containerCard = document.querySelector('.cards');
 
  
 fetch(URL)
     .then(function (response) {
         return response.json();
     })
-    .then(function (jsonObject) {
-        console.table(jsonObject);  // temporary checking for valid response and data parsing
+    .then(function (jsonObject) { 
         const prophets = jsonObject['prophets'];
         prophets.forEach(displayProphets);
     });
@@ -16,20 +15,26 @@ function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let dateBirth = document.createElement('p');
+    let placeBirth = document.createElement('p');
     let portrait = document.createElement('img');
 
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = prophet.name + ' ' + prophet.lastname;
+    dateBirth.textContent = 'Date of Birth:'+ ''+ prophet.birthdate;
+    placeBirth.textContent = 'place of Birth:'+ ''+prophet.birthplace;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    ____.setAttribute('loading', 'lazy');
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(dateBirth);
+    card.appendChild(placeBirth);
     card.appendChild(portrait);
 
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
+    containerCard.appendChild(card);
 }
