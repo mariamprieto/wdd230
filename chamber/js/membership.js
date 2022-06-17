f57838126a7d58d141cc2a5ee4ef0a8af778aed0
mariamprieto <mariaprieto@byui.edu>
2022-06-17 14:06:41 -0400

    Udpate join page

--- chamber/js/membership.js
+++ chamber/js/membership.js
@@ -1,26 +1,53 @@
 function changeStatus() {
-  var status_member = document.getElementById("membership");
-  
-  
-  if (status_member.value === "nonprofit") {
-    document.getElementById("nonprofit").style.display = "block";
+  const status_member = document.getElementById("membership");
+  const nonprofit = document.getElementById("nonprofit");
+  const bronze = document.getElementById("bronze");
+  const silver = document.getElementById("silver");
+  const gold = document.getElementById("gold");
+  const benefits = document.getElementById("benefits");
 
-  }
-  if (status_member.value === "bronze") {
-    document.getElementById("bronze").style.display = "block";
-  }
-  if (status_member.value === "silver") {
-    document.getElementById("silver").style.display = "block";
-  }
-  if (status_member.value === "gold") {
-    document.getElementById("gold").style.display = "block";
-  }
-  else {
-    document.getElementById("benefits").style.display = "none";
-  }
+  switch (status_member.value) {
+    case 'select':
+      benefits.style.display = 'none';
+      nonprofit.style.display = 'none';
+      bronze.style.display = 'none';
+      silver.style.display = 'none';
+      gold.style.display = 'none';
+      break;
+    case 'nonprofit':
+      benefits.style.display = 'block';
+      nonprofit.style.display = 'block';
+      bronze.style.display = 'none';
+      silver.style.display = 'none';
+      gold.style.display = 'none';
+      break;
+    case 'bronze':
+      benefits.style.display = 'block';
+      nonprofit.style.display = 'none';
+      bronze.style.display = 'block';
+      silver.style.display = 'none';
+      gold.style.display = 'none';
+      break;
+    case 'silver':
+      benefits.style.display = 'block';
+      nonprofit.style.display = 'none';
+      bronze.style.display = 'none';
+      silver.style.display = 'block';
+      gold.style.display = 'none';
+      break;
+    case 'gold':
+      benefits.style.display = 'block';
+      nonprofit.style.display = 'none';
+      bronze.style.display = 'none';
+      silver.style.display = 'none';
+      gold.style.display = 'block';
+      break;
 
+    default:
+      benefits.style.display = "none";
 
-
+    
+  }
 
 
 }
