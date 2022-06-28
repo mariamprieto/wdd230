27a66ccb54881944a5287a55c7fcc19446c9cf8f
mariamprieto <mariaprieto@byui.edu>
2022-06-28 17:26:43 -0400

    Update

--- chamber2/js/menu.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("open");
-  document.getElementById("hamburger").classList.toggle("open");
-}
-
-const x = document.getElementById("hamburger");
-x.onclick = toggleMenu;
