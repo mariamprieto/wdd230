64dc9fb9e8fd3823dcc3ad59aab83ccf79ef2f46
mariamprieto <mariaprieto@byui.edu>
2022-06-29 01:44:03 -0400

    udpate

--- chamber3/js/message.js
+++
@@ -1,9 +0,0 @@
-const message = document.querySelector(".message");
-const day = new Date().getDay();
-if (day === 1 || day === 2) {
-  message.textContent =
-    "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-} else {
-  message.textContent =
-    "We're sorry you missed the weekly chamber meet and greet but feel free to join us next Wednesday at 7:00 p.m.";
-}
