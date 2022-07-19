6ad8a2d6a5df28fc2f1fda78f080e841ae01ea38
mariamprieto <mariaprieto@byui.edu>
2022-07-19 13:06:25 -0400

    Update

--- final_project/js/menu.js
+++ final_project/js/menu.js
@@ -1,3 +1,27 @@
+const button_menu = document.querySelector("#btonMenu");
+const menu = document.querySelector("#menu");
 
+button_menu.addEventListener("click", function () {
+    menu.classList.toggle("display");
+});
 
+const subMenuBtn = document.querySelectorAll(".submenu_btn");
+for (let i = 0; i < subMenuBtn.length; i++) {
+    subMenuBtn[i].addEventListener("click", function () {
+        if (window.innerWidth < 1024) {
+            
+            const subMenu = this.nextElementSibling;
+            const height = subMenu.scrollHeight;
 
+            if (subMenu.classList.contains("deploy")) {
+                subMenu.classList.remove("desploy");
+                subMenu.removeAttribute("style");
+            }
+            else {
+                subMenu.classList.add("desploy");
+                subMenu.style.height = height + "px";
+            }
+        }
+    });
+
+}
