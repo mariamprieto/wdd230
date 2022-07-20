d8daeeb445bef48375654eb6292793e3f63b2032
mariamprieto <mariaprieto@byui.edu>
2022-07-20 11:09:00 -0400

    Update

--- final_project/js/menu.js
+++ final_project/js/menu.js
@@ -1,29 +1,18 @@
-const button_menu = document.querySelector("#btonMenu");
-const menu = document.querySelector("#menu");
 
-button_menu.addEventListener("click", function () {
-    menu.classList.toggle("display");
-});
+const btnMobile = document.getElementById('btn-mobile');
 
-
-
-const subMenuBtn = document.querySelectorAll(".submenu_btn");
-for (let i = 0; i < subMenuBtn.length; i++) {
-    subMenuBtn[i].addEventListener("click", function () {
-        if (window.innerWidth < 1024) {
-            
-            const subMenu = this.nextElementSibling;
-            const height = subMenu.scrollHeight;
-
-            if (subMenu.classList.contains("deploy")) {
-                subMenu.classList.remove("desploy");
-                subMenu.removeAttribute("style");
-            }
-            else {
-                subMenu.classList.add("desploy");
-                subMenu.style.height = height + "px";
-            }
-        }
-    });
-
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Close Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Open Menu');
+    }
 }
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
